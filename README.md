a66f2300c31a862299d133e7501c791fd5f163df
Fabrizio Riguzzi <fabrizio.riguzzi@unife.it>
2016-11-24 07:35:48 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# cplint-datasets
-Datasets for cplint
+# cplint_datasets
+Datasets for machine learning with cplint https://github.com/friguzzi/cplint
---
+++ pack.pl
@@ -0,0 +1,5 @@
+name(cplint_datasets).
+title('Dataset for machine learning with cplint').
+version('1.0').
+author('Fabrizio Riguzzi','fabrizio.riguzzi@unife.it').
+
